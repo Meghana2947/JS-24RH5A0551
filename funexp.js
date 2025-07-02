efffa50fca9d59c90b77112c4992234d23fa8a80
Meghana2947 <reddypasulameghana@gmail.com>
2025-07-02 12:45:39 +0530

    Add files via upload

---
+++ funexp.js
@@ -0,0 +1,13 @@
+function welcomeMessage(){
+    console.log("Welcome to MRECW");
+}
+function add(a,b){
+    return a+b;
+}
+
+
+const mul=(a,b)=>(a*b);
+console.log(mul(5,6));
+
+const greet=()=>("Haii");
+console.log(greet());
---
+++ object.js
@@ -0,0 +1,5 @@
+let user={name:"Meghana",dept:"CSE",age:20};
+console.log(user);
+console.log(user.name);
+console.log(user.dept);
+console.log(user.age);
